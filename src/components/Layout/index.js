4f5850b7762f86a1c46902d17a37921e099807a0
Rishav Sharma <rishabhtonic2000@gmail.com>
2022-04-13 16:40:57 +0530

    First setup

---
+++ src/components/Layout/index.js
@@ -0,0 +1,10 @@
+import { Sidebar } from '../Sidebar';
+import './index.scss';
+
+export const Layout = () => {
+    return (
+        <>
+           <Sidebar />
+        </>
+    )
+}
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+import './index.scss';
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+
+export const Sidebar = () => (
+
+    <div className="nav-bar">
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt="logo"/>
+            <img src={LogoSubtitle} alt="logo subtitle"/>
+        </Link>
+    </div>
+)
